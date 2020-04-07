a601267a4db59a639457a964733b4bac992e993b
Ashot-Petrosyan <61912592+Ashot-Petrosyan@users.noreply.github.com>
2020-04-07 17:45:50 +0300

    Updates

---
+++ precache-manifest.cd6e6e128675a360e007b8087cf65b34.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359f270796b386f707530b47fa5a52cb",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "5c0bca1f76f9b4f568c6",
+    "url": "/weather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ddafda8d1f54da7bfc27",
+    "url": "/weather/static/css/main.2d39e5a7.chunk.css"
+  },
+  {
+    "revision": "5c0bca1f76f9b4f568c6",
+    "url": "/weather/static/js/2.657f669a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.657f669a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddafda8d1f54da7bfc27",
+    "url": "/weather/static/js/main.2f7b9258.chunk.js"
+  },
+  {
+    "revision": "271d1cdfd5dced67f6eb",
+    "url": "/weather/static/js/runtime-main.6cdf629c.js"
+  },
+  {
+    "revision": "705533f49a40effcddb1b318d92ca2ff",
+    "url": "/weather/static/media/city.705533f4.jpg"
+  }
+]);
